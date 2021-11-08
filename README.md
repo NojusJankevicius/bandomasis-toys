a6c21342990f3b27769f33d09142a5f2a8e6b11a
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2021-11-08 14:04:41 +0200

    [4]. API structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bandomasis-toys
+
+# required programs
+node.js
+
+## instalation
+npm i
+
+## start program
+* npm start
---
+++ js/API.js
@@ -0,0 +1,28 @@
+const url = "http://localhost:3000";
+
+class API {
+    static getToys = (success, failure) => {
+        fetch(`${url}/Toy`)
+        .then( res => res.json())
+        .then(success)
+        .catch(failure)
+    }
+    
+    static deleteToys = (id, success, failure) => {
+        fetch(`${url}/Toy/${id}`, {method: 'DELETE'})
+        .then( res => res.json())
+        .then(success)
+        .catch(failure)
+    }
+}
+
+// API.getToys(
+//     (data) => console.log("data received:", data),
+//     (error) => console.error(error)
+// )
+
+// API.deleteToys(
+//     '5210dfx',
+//     (data) => console.log("data received:", data),
+//     (error) => console.error(error)
+// )
